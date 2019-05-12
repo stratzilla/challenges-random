fb95e6fcdbb2765410af7d2dabc44e9722649561
Robin Scott <stratzillab@gmail.com>
2019-05-12 01:03:11 -0400

    Update repeatingsubstrings.py

--- repeatingsubstrings.py
+++ repeatingsubstrings.py
@@ -1,3 +1,24 @@
+'''
+Determine if a string contains strictly
+a repetition of any substrings
+
+a string contains only substring repetitions
+if you can shift it and get the same string
+in under len shifts
+
+for example:
+testtest - 0 shifts, original string
+esttestt - 1 shift, not original
+sttestte - 2 shifts, not original
+ttesttes - 3 shifts, not original
+testtest - 4 shifts, original again
+
+since 4 < len, the string only contains substring
+repetitions. But consider "testteste"; this will
+only become the original after len shifts, meaning
+it does not contain strictly substring repetitions
+'''
+
 def onlySubstrings(stringOrig):
 	stringCopy = (stringOrig+'.')[:-1] # copy string contents
 	for i in range(1, len(stringCopy)): # for each character
@@ -8,7 +29,14 @@ def onlySubstrings(stringOrig):
 		return False
 
 if __name__ == '__main__':
-    wordList = ['testtest','testteste','test','tetestst']
-    print('The below words only contain substrings:')
-    for word in wordList:
+    wordList = ['testtest','testteste','test','tetestst'] # some test words
+    print('The below words only contain substring repetitions:')
+    for word in wordList: # for each word
         print(onlySubstrings(word), '\t', word)
+	
+```
+True	testtest
+False	testteste
+False	test
+False	tetestst
+```
