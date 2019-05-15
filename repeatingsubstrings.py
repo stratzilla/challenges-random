f549cdd8058ed5abc04531100287dcfe774865f7
Robin Scott <stratzillab@gmail.com>
2019-05-14 21:54:23 -0400

    Update repeatingsubstrings.py

--- repeatingsubstrings.py
+++ repeatingsubstrings.py
@@ -25,22 +25,24 @@ only become the original after len shifts, meaning
 it does not contain strictly substring repetitions
 '''
 
-def shiftString(str, n):
+def contains_substring_repetitions(str):
+	# test whether shifted string equals
+	for i in range(1, len(str)):
+		if shift_string(str, i) == str: # if equivalent to original input 
+			return True
+		elif i == int(len(str)) + 1: # can only contain substrings at most len length
+			return False
+	return False
+
+def shift_string(str, n):
 	# shift first char to end
 	return str[n:] + str[:n]
 
-def onlySubstrings(str):
-	# test whether shifted string equals
-	for i in range(1, len(str)):
-		if shiftString(str, i) == str: # if equivalent to original input 
-			return True # it consists of substrings only
-	return False # otherwise the only substring is the string itself
-
 if __name__ == '__main__':
-    	wordList = ['testtest','testteste','test','tetestst'] # some test words
+    	word_list = ['testtest','testteste','test','tetestst'] # some test words
     	print('The below words only contain substring repetitions:')
-    	for word in wordList: # for each word
-		print(onlySubstrings(word), '\t', word)
+    	for word in word_list: # for each word
+		print(contains_substring_repetitions(word), '\t', word)
 	
 '''
 True	testtest
