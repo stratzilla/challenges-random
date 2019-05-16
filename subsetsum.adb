66dc8010c66de5ad42cab9357e09e052da64f03a
Robin Scott <stratzillab@gmail.com>
2019-05-16 11:38:21 -0400

    Update subsetsum.adb

--- subsetsum.adb
+++ subsetsum.adb
@@ -10,20 +10,17 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Command_line; use Ada.Command_Line;
 
-procedure Main is
-
+procedure Subset_Sum is
     -- a set of numbers to find subset sum on
     type Set is array(0..4) of Integer;
     Arr : Set;
-    -- boolean values used later for recursion
-    A, B : Boolean;
-    
+
     -- find if subset sum exists in a set
     -- @param Arr - the set to check
     -- @param S - the sum to check against Arr
     -- @param N - for iteration; equals Arr.length
     -- @returns - truth value on whether subset sum exists
-    function SubsetSum (Arr : Set; S, N : Integer) return Boolean is
+    function Find_Subset (Arr : in Set; S, N : in Integer) return Boolean is
     begin
         -- a sum of zero is always possible
         if S = 0 then
@@ -34,56 +31,48 @@ procedure Main is
         end if;
         -- if last is bigger than sum, ignore and go deeper
         if (Arr(N-1)) > S then
-            return SubsetSum(Arr, S, N-1);
+            return Find_Subset(Arr, S, N-1);
         end if;
         -- check including the last
-        A := SubsetSum(Arr, S, N-1);
         -- and excluding the last
-        B := SubsetSum(Arr, S-Arr(N-1), N-1);
-        return A or B;
-    end SubsetSum;
+        return Find_Subset(Arr, S, N-1) 
+            or else Find_Subset(Arr, S-Arr(N-1), N-1);
+    end Find_Subset;
     
-    -- verifies arguments are correct before
-    -- continuing
+    -- verifies arguments are correct before continuing
     -- @returns - truth value on whether args valid
-    function VerifyArgs return Boolean is
+    function Verify_Args return Boolean is
     begin
         -- only six arguments allowed
         -- one for sum, five for set
         if Argument_Count /= 6 then
             -- instruct user how to use args
-            Put("Invalid arguments.");
-            New_Line;
-            Put("Use arguments 's a..e'.");
-            New_Line;
-            Put("s = sum to check, a..e = the five elements of the set.");
-            New_Line;
-            Put("For example, '12 5 2 6 3 4':");
-            Put("Will check for subset sum of 12 in {5, 2, 6, 3, 4}");
+            Put("Invalid arguments."); New_Line;
+            Put("Use arguments '<arg1> <args2>'."); New_Line;
+            Put("<arg1> is the sum to find, <args2> is space delimited list of 5 numbers.");
             return False;
         end if;
         return True;
-    end VerifyArgs;
+    end Verify_Args;
     
 begin
     -- if valid arguments
-    if VerifyArgs = True then
-        Put("Checking {");
+    if Verify_Args then
+        Put("{");
         -- populate Arr with arguments
         for i in 0..4 loop
             Arr(i) := Integer'value(Argument(i+2));
             Put(Integer'Image(Arr(i)));
-            if i /= Argument_Count then
+            if i /= 4 then
                 Put(", ");
             end if;
         end loop;
-        Put("} for Subset Sum of " & Argument(1));
-        New_Line;
+        Put("} ");
         -- determine if a subset sum exists
-        if SubsetSum(Arr, Integer'Value(Argument(1)), Arr'Length) = False then
-            Put("There is no subset sum.");
+        if Find_Subset(Arr, Integer'Value(Argument(1)), Arr'Length) = False then
+            Put("does not contain subset sum " & Argument(1));
         else
-            Put("There is a subset sum.");
+            Put("contains subset sum " & Argument(1));
         end if;
     end if;    
-end Main;
+end Subset_Sum;
